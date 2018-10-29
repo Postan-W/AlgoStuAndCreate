bec3dc2b6b7432650aafa87e00d4ef0ff3b7a16e
Shiyi Wang <wangmingzhu@qknode.com>
2018-10-29 18:30:36 +0800

    图的广度与深度遍历

--- src/PublicClass.java
+++ src/PublicClass.java
@@ -7,61 +7,12 @@ import java.util.Arrays;
 
 import static java.lang.System.*;
 public class PublicClass implements Demo{
+
     public static void main(String []args){
-        int [] a = {1,3,2,14,5,10,8};
-      //  HeapSort.heapSort(a,a.length);
-        HeapSort.heapSort(a,a.length);
-        //System.out.println(Arrays.toString(heap.getArray()));
+      char [] data = {'a','b','c','d'};
+      int [][] weight = {{0,1,0,1},{1,0,1,1},{0,1,0,0},{1,1,0,0}};
+      Graph graph = new Graph(data,weight);
+      graph.BFS(0);
     }
-    static class Employee {
-        private String name;
-        private int age;
-        private String position;
-        private float salary;
-
-        public Employee(String n, int a, String p,float s){
-            name = n;
-            age = a;
-            position = p;
-            salary = s;
-        }
-        public boolean equals(Employee e){
-            if(e.getAge() == this.getAge()){
-                return true;
-            }
-            return false;
-        }
-            public String getName() {
-                return name;
-            }
-
-            public void setName(String name) {
-                this.name = name;
-            }
-
-            public int getAge() {
-                return age;
-            }
-
-            public void setAge(int age) {
-                this.age = age;
-            }
-
-            public String getPosition() {
-                return position;
-            }
-
-            public void setPosition(String position) {
-                this.position = position;
-            }
-
-            public float getSalary() {
-                return salary;
-            }
-
-            public void setSalary(float salary) {
-                this.salary = salary;
-            }
-        }
 }
 
---
+++ src/util/Graph.java
@@ -0,0 +1,98 @@
+package util;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static java.lang.System.*;
+/**
+ @author  Shiyi Won
+ @time 2018.10.29
+ @theme 图的定义及深度与广度遍历，基于临接矩阵,时间复杂度O(n的平方)
+ */
+//变量均公开
+public class Graph {
+    public int vertices_num;
+    public char data[];
+    public int weight[][];//存储边与边的关系
+    public int [] visited;//这个数组的作用是保存已经被访问过的节点
+
+    public Graph(char data[],int weight[][]){
+        this.vertices_num = data.length;
+        this.data = new char[vertices_num];
+        this.visited = new int[vertices_num];
+        this.weight = new int[vertices_num][vertices_num];
+        for (int i =0; i<data.length;i++){
+            this.data[i] = data[i];
+        }
+        for (int j = 0;j<data.length;j++){
+            for (int k=0;k<data.length;k++){
+                this.weight[j][k] = weight[j][k];
+            }
+        }
+    }
+
+    //获取某节点的第一个相邻点
+    public int getFirst(int k) throws ArrayIndexOutOfBoundsException{
+        if( k <0 || k>this.vertices_num -1){
+            throw new ArrayIndexOutOfBoundsException();
+        } else {
+            for (int i = 0;i<this.vertices_num;i++){
+                //节点自己与自己的weight值设置为0
+                if (this.weight[k][i] == 1){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    //获取节点k的相邻节点j之外的另一个相邻节点
+    public int getNext(int k, int j) throws ArrayIndexOutOfBoundsException{
+        if ( k<0 || k>this.vertices_num -1 || j <0 || j>this.vertices_num){
+            throw new ArrayIndexOutOfBoundsException();
+        } else {
+            for (int i = j+1;i<this.vertices_num;i++){
+                if (this.weight[k][i] == 1){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+    //深度优先遍历图,参数k是某个节点的序号
+    //DFS的思路是访问某个节点及其相邻节点，直到某个节点没有相邻节点，然后回退到上一个节点的相邻节点，重复这个过程
+    public void DFS(int k){
+        int u; //节点k的邻接节点
+        out.print(this.data[k] + "-->");
+        visited[k] = 1;
+        u = getFirst(k);
+        while (u != -1) {
+        if (visited[u] ==0){
+            DFS(u);//从这里开始深度预先遍历相邻节点
+        }
+        u = getNext(k,u);//相邻节点遍历完后再回到下一个相邻节点
+        }
+        }
+        //广度优先遍历的思路是先遍历一个节点的所有节点，然后依次对它所有的节点进行这样的操作
+    /**根据队列先进先出的特点，一个节点的相邻节点依次访问，那么它的相邻节点的相邻节点也具有这样的先后次序
+       与队列先进先出的特点一致.
+        代码实现的思路是当队列不为空时，取出一个节点访问他，然后把他所有的节点依次进队列知道没有相邻节点，
+        然后再取出头部那个节点，重复这样的操作。因为一个节点的相邻节点也有可能相邻，所以也要判断是否被访问过
+     */
+    public void BFS(int k){
+        Queue<Integer> queue = new LinkedList<Integer>();//linkedlist既可以作队列㛑可以作栈，这取决于引用的类型
+        int u;
+        queue.add(k);
+        this.visited[k] = 1;
+        while(!queue.isEmpty()){
+            u = queue.remove();
+            out.print(data[u] + "-->");
+            int v = getFirst(u);
+            while(v !=-1){
+                if (visited[v]!=1){
+                    queue.add(v);
+                    visited[v] =1;
+                 }
+                 v = getNext(u,v);
+            }
+        }
+    }
+}
